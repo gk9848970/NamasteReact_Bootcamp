8cb9eba62cd01375597716cefc3e94fc854d8e4b
Gaurav <gk9848970@gmail.com>
2022-12-10 18:15:28 +0530

    Ran react through npm package via parcel bundler

--- Day 2/Code/src/App.js
+++ Day 2/Code/src/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement('h1', { id: 'title' }, 'Namaste react');
+// const heading = React.createElement('h1', { id: 'title' }, 'Namaste react');
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+
+const heading = <div>
+    <h1>Namaste react</h1>
+    <h2>Namaste react</h2>
+</div>;
+
+const nummber = 
+
+
+
+10;
+
+function TestComponent() {
+    return (
+        <div>{heading}</div>
+    );
+}
+root.render(<TestComponent />);
