c9c4f9ee20d09b016cfd1eba6d97c7e4014ab910
Gaurav <gk9848970@gmail.com>
2022-12-10 18:23:15 +0530

    Ran react through npm

--- Day 2/Code/src/App.js
+++ Day 2/Code/src/App.js
@@ -3,21 +3,5 @@ import ReactDOM from 'react-dom/client';
 
 // const heading = React.createElement('h1', { id: 'title' }, 'Namaste react');
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const heading = <div>
-    <h1>Namaste react</h1>
-    <h2>Namaste react</h2>
-</div>;
-
-const nummber = 
-
-
-
-10;
-
-function TestComponent() {
-    return (
-        <div>{heading}</div>
-    );
-}
-root.render(<TestComponent />);
+const heading = React.createElement('h1', {}, "Hello World");
+root.render(heading);
